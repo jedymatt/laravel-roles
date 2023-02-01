8774ae3978727ef74a09a179e3acdf1be575baa2
Jedy Matt Tabasco <jedymatt@gmail.com>
2023-02-01 13:32:25 +0800

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Models\User;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -15,4 +16,13 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
+});
+
+Route::get('/login', function () {
+    $user = User::first();
+
+    auth()->login($user);
+    request()->session()->regenerate();
+
+    return 'Logged in';
 });
