13d40b33aab99e829b79655b1911adb9b9ddb490
Jedy Matt Tabasco <jedymatt@gmail.com>
2023-02-01 13:59:03 +0800

    Add role method

--- app/Models/User.php
+++ app/Models/User.php
@@ -41,4 +41,12 @@ class User extends Authenticatable
     protected $casts = [
         'email_verified_at' => 'datetime',
     ];
+
+    /**
+     * Get the role that owns the user.
+     */
+    public function role()
+    {
+        return $this->belongsTo(Role::class);
+    }
 }
