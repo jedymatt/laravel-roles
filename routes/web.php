8e6959d58010a0899bee0ead84f0e9b7a0927c87
Jedy Matt Tabasco <jedymatt@gmail.com>
2023-02-01 12:52:40 +0800

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Models\Role;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -16,3 +17,9 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/home', function () {
+
+    auth()->user()->roles()->sync(Role::first());
+    dd(auth()->user()->toArray(), auth()->user()->roles->toArray());
+})->middleware(['auth.basic'])->name('home');
