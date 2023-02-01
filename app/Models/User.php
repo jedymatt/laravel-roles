d8c11b737960b522e1d08707c17c753f81eedf45
Jedy Matt Tabasco <jedymatt@gmail.com>
2023-02-01 13:07:27 +0800

    Update User.php

--- app/Models/User.php
+++ app/Models/User.php
@@ -41,4 +41,12 @@ class User extends Authenticatable
     protected $casts = [
         'email_verified_at' => 'datetime',
     ];
+
+    /**
+     * Get the roles for the user.
+     */
+    public function roles()
+    {
+        return $this->belongsToMany(Role::class);
+    }
 }
